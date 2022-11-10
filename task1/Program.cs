bcf01792c452553236bad38b81c0561c7d3d61bc
Artem <temazasport@mail.ru>
2022-11-10 16:44:34 +0800

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿var random = new Random();
+
+int num = random.Next(10,100);
+System.Console.WriteLine($"Число {num}");
+int numFirst = num / 10;
+int NumSecond = num % 10;
+
+int max = 0;
+
+if(numFirst<NumSecond){
+    max = NumSecond;
+}
+else{
+    max = numFirst;
+}
+Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
+
