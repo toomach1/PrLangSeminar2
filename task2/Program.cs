58319fc441680d465541869d64def1d4ac7e8f5a
Artem <temazasport@mail.ru>
2022-11-14 10:33:32 +0800

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿var random = new Random();
+
+int num = random.Next(100,1000);
+System.Console.WriteLine($"Число {num}");
+int numFirst = num / 100;
+int NumSecond = num % 10;
+int result = numFirst * 10 + NumSecond;
+
+Console.WriteLine($"Итоговое число в числе {result}");
