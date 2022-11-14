1b7ea1cd5c7da4cca675c3171417c7d44544eea0
Artem <temazasport@mail.ru>
2022-11-14 13:33:07 +0800

    HW_task 2

---
+++ HW_task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+string NumText = Convert.ToString(Num);
+if (Num > 99){
+  Console.WriteLine("третья цифра -> " + NumText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
