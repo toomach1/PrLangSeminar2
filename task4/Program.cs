191c285cb01383ec776ee60e0336fb1ef4304a59
Artem <temazasport@mail.ru>
2022-11-14 11:25:42 +0800

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введитe число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result1 = num%7;
+int result2 = num%23;
+
+if(result1 == 0 && result2 == 0){ 
+    
+    Console.WriteLine("Да");
+ 
+}
+
+else{
+    Console.WriteLine("Нет");
+}
