199467b323786ebcd559a0d52b6715da925e1f7f
Artem <temazasport@mail.ru>
2022-11-14 12:12:22 +0800

    task 5

---
+++ HW_task1/Program.cs
@@ -0,0 +1,9 @@
+﻿var random = new Random();
+
+int num = random.Next(100,1000);
+System.Console.WriteLine($"Число {num}");
+int numFirst = num / 10;
+int NumSecond = numFirst % 10;
+
+
+Console.WriteLine(NumSecond);
