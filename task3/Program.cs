4b36a2d85aafe65fb607de960b749a0c0d171693
Artem <temazasport@mail.ru>
2022-11-14 10:51:10 +0800

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 2 числa");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num%num2;
+
+if(result == 0){
+    
+    Console.WriteLine("Кратно");
+ 
+}
+
+else{
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
+
